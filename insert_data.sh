1ac355755c85fef16b7bfb0be9cf510fded0e9b0
Daniel Polukchi <danilabodrov746@gmail.com>
2024-10-08 04:21:50 +0000

    Save progress: 910.1

--- insert_data.sh
+++ insert_data.sh
@@ -45,6 +45,6 @@ do
     fi
 
     # insert into majors_courses
-
+    INSERT_MAJORS_COURSES_RESULT=$($PSQL "INSERT INTO majors_courses(major_id, course_id) VALUES ('$MAJOR_ID', '$COURSE_ID')")
   fi
 done
