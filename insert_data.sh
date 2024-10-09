c904a4314aa2230fb30a4b12d54de848c6a71dec
Daniel Polukchi <danilabodrov746@gmail.com>
2024-10-09 04:27:58 +0000

    Save progress: 1020.1

--- insert_data.sh
+++ insert_data.sh
@@ -52,7 +52,7 @@ do
   fi
 done
 
-cat students_test.csv | while IFS="," read FIRST LAST MAJOR GPA
+cat students_test.csv | while IFS="," read STUDENT FIRST LAST MAJOR GPA
 do
-  echo $FIRST
+  echo  $STUDENT
 done
